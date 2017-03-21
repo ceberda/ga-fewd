705b88adce37be81ba589c24511c3f8f61886b2e
ceberda <ceberda@gmail.com>
2017-03-21 08:12:25 -0400

    week 9 homework

---
+++ d09/materials/travelling-dwarf/solution.js
@@ -0,0 +1,45 @@
+console.log("WRITE YOUR SOLUTION IN HERE!");
+
+var box = 0;
+
+ for(box = 0; box < 4; box++){
+ d.move();
+
+}
+
+d.orient("down");
+
+ for(box = 0; box < 16; box++){
+ d.move();
+
+}
+d.orient("right");
+
+ for(box = 0; box < 6; box++){
+ d.move();
+
+}
+d.orient("up");
+
+for(box = 0; box < 11; box++){
+ d.move();
+
+}
+d.orient("right");
+
+for(box = 0; box < 6; box++){
+ d.move();
+
+}
+d.orient("down");
+
+for(box = 0; box < 9; box++){
+ d.move();
+
+}
+d.orient("right");
+
+for(box = 0; box < 2; box++){
+ d.move();
+
+}
